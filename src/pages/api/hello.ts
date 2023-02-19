b0d4601520b01daf935e5e7dada91945c7960b46
metrox-coder <paul.kirpur@gmail.com>
2023-02-18 17:22:47 -0800

    dafaf

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-
+import { PrismaClient } from ".prisma/client";
+import { Prisma } from "@prisma/client";
+import type { NextApiRequest, NextApiResponse } from "next";
+const prisma = new PrismaClient();
 type Data = {
-  name: string
-}
+  name: string;
+};
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  await prisma.user.create({ data: { name: "", email: "" } });
+  res.status(200).json({ name: "John Doe" });
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
-import { Inter } from '@next/font/google';
+import { Inter } from "@next/font/google";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const test = '123123';
+  const test = "123123";
 
   return (
-    <div className='text-xl'>
+    <div className="text-xl">
       <p>asdasdasd</p>
       <p>{test}</p>
     </div>
