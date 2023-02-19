0f219e7ab918e8ac3d971bd5f2e826e81842cca4
Don-Cryptus <don.cryptus@gmail.com>
2023-02-19 02:35:54 +0100

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,12 +4,13 @@ import { useState } from 'react';
 export default function Home() {
   const [name, setName] = useState('don');
   const [email, setEmail] = useState(
-    'don@don.don' + `${Math.random()}`.slice(15)
+    `${Math.random()}`.slice(15) + 'don@don.don'
   );
 
   const submit = async () => {
     const res = await axios.post('/api/create-user', { name, email });
     console.log(res.data);
+    setEmail(`${Math.random()}`.slice(15) + 'don@don.don');
   };
 
   return (
